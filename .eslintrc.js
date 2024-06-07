0903c8a208d1ac2b16761d3d3c5a820b128aa180
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2024-06-06 21:39:51 -0300

    chore: configura projeto
    Este commit contempla:
    
    - Instalação das dependências do projeto
    - Configuração do ESLint
    - Configuração do nodemon
    - Configuração do TypeScript

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+    rules: {
+        indent: [
+            "error",
+            4,
+            {
+                ignoredNodes: ["PropertyDefinition"],
+            },
+        ],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                args: "all",
+                argsIgnorePattern: "^_",
+                caughtErrors: "all",
+                caughtErrorsIgnorePattern: "^_",
+                destructuredArrayIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+                ignoreRestSiblings: true,
+            },
+        ],
+    },
+};
